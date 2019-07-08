7bcfbe949cd0d6945c3a2a8eacf6f6db32c67147
GodsWithin <robin.de.zwaef@telenet.be>
2019-07-08 16:54:33 +0200

    Optimize Downloader resolving

--- KoreanNewsDownloader/Downloaders/DownloaderResolver.cs
+++ KoreanNewsDownloader/Downloaders/DownloaderResolver.cs
@@ -1,6 +1,7 @@
 ﻿using KoreanNewsDownloader.Downloaders;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 
 namespace KoreanNewsDownloader
 {
@@ -20,59 +21,14 @@ namespace KoreanNewsDownloader
 
         public IDownloader GetDownloaderByName(string host)
         {
-            switch (host)
+            IDownloader downloader = _services.GetServices<IDownloader>().Where(x => x.HostUrls.Contains(host)).FirstOrDefault();
+
+            if (downloader == null)
             {
-                case "www.ajunews.com": return _services.GetRequiredService<AjunewsDownloader>();
-                case "www.asiatoday.co.kr": return _services.GetRequiredService<AsiatodayDownloader>();
-                case "biz.heraldcorp.com":
-                case "pop.heraldcorp.com": return _services.GetRequiredService<HeraldcorpDownloader>();
-                case "bntnews.hankyung.com":
-                case "www.hankyung.com":
-                case "news.hankyung.com":
-                case "tenasia.hankyung.com": return _services.GetRequiredService<HankyungDownloader>();
-                case "www.breaknews.com": return _services.GetRequiredService<BreaknewsDownloader>();
-                case "www.getnews.co.kr":
-                case "cnews.getnews.co.kr": return _services.GetRequiredService<GetnewsDownloader>();
-                case "www.thebigdata.co.kr":
-                case "cnews.thebigdata.co.kr": return _services.GetRequiredService<ThebigdataDownloader>();
-                case "www.dailian.co.kr": return _services.GetRequiredService<DailianDownloader>();
-                case "www.dailypop.kr": return _services.GetRequiredService<DailypopDownloader>();
-                case "www.dispatch.co.kr": return _services.GetRequiredService<DispatchDownloader>();
-                case "www.dt.co.kr": return _services.GetRequiredService<DtDownloader>();
-                case "www.edaily.co.kr": return _services.GetRequiredService<EdailyDownloader>();
-                case "www.egn.kr": return _services.GetRequiredService<EgnDownloader>();
-                case "enews.imbc.com": return _services.GetRequiredService<ImbcDownloader>();
-                case "enews24.tving.com": return _services.GetRequiredService<TvingDownloader>();
-                case "enter.etoday.co.kr":
-                case "www.etoday.co.kr": return _services.GetRequiredService<EtodayDownloader>();
-                case "entertain.v.daum.net": return _services.GetRequiredService<DaumDownloader>();
-                case "www.fnnews.com":
-                case "star.fnnews.com": return _services.GetRequiredService<FnnewsDownloader>();
-                case "gall.dcinside.com": return _services.GetRequiredService<DcinsideDownloader>();
-                case "www.getitk.com": return _services.GetRequiredService<GetitkDownloader>();
-                case "www.ilyoseoul.co.kr": return _services.GetRequiredService<IlyoseoulDownloader>();
-                case "www.inews24.com": return _services.GetRequiredService<Inews24Downloader>();
-                case "www.insight.co.kr":
-                case "photo.insight.co.kr": return _services.GetRequiredService<InsightDownloader>();
-                case "www.intronews.net": return _services.GetRequiredService<IntronewsDownloader>();
-                case "isplus.live.joins.com":
-                case "dcnewsj.joins.com": return _services.GetRequiredService<JoinsDownloader>();
-                case "www.issuedaily.com": return _services.GetRequiredService<IssuedailyDownloader>();
-                case "www.joynews24.com": return _services.GetRequiredService<Joynews24Downloader>();
-                case "www.kookje.co.kr": return _services.GetRequiredService<KookjeDownloader>();
-                case "www.kukinews.com": return _services.GetRequiredService<KukinewsDownloader>();
-                case "www.liveen.co.kr": return _services.GetRequiredService<LiveenDownloader>();
-                case "post.naver.com":
-                case "m.post.naver.com": return _services.GetRequiredService<NaverDownloader>();
-                case "sports.chosun.com":
-                case "m.sportschosun.com": return _services.GetRequiredService<ChosunDownloader>();
-                case "www.mdpr.jp":
-                case "mdpr.jp":  return _services.GetRequiredService<MdprDownloader>();
-                case "www.mediasr.co.kr": return _services.GetRequiredService<MediasrDownloader>();
-                case "www.metroseoul.co.kr": return _services.GetRequiredService<MetroseoulDownloader>();
+                throw new ArgumentException($"'{host}' is not a supported website.");
             }
 
-            throw new ArgumentException($"'{host}' is not a supported website.");
+            return downloader;
         }
     }
 }
